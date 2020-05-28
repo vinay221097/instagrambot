2e9cf51eaa60103d3078a35aa231a149669359b1
vinay221097 <vinay221097@gmail.com>
2020-05-28 08:10:17 +0530

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -15,11 +15,17 @@ session = InstaPy(username='smithjohn4321234', password='abcd4321@',headless_bro
 # let's go! :>
 with smart_run(session):
     # general settings
-    session.set_quota_supervisor(enabled=True, sleep_after=['likes', 'comments_d', 'follows', 'unfollows', 'server_calls_h'], sleepyhead=True, stochastic_flow=True, notify_me=True,peak_likes=(57, 585),
-                               peak_comments=(21, 182),
-                                peak_follows=(48, None),
-                                 peak_unfollows=(35, 402),
-                                  peak_server_calls=(None, 4700))
+    session.set_quota_supervisor(enabled=True, sleep_after=["likes", "comments_d", "follows", "unfollows", "server_calls_h"], sleepyhead=True, stochastic_flow=True, notify_me=True,
+                              peak_likes_hourly=57,
+                              peak_likes_daily=585,
+                               peak_comments_hourly=21,
+                               peak_comments_daily=182,
+                                peak_follows_hourly=48,
+                                peak_follows_daily=None,
+                                 peak_unfollows_hourly=35,
+                                 peak_unfollows_daily=402,
+                                  peak_server_calls_hourly=None,
+                                  peak_server_calls_daily=4700)
     session.set_relationship_bounds(enabled=True,
                                     potency_ratio=-1.3,
                                     delimit_by_numbers=True,
